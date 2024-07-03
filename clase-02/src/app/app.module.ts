8f242a2b1757a60d39a062c783d68e35cd125d00
julieta <julisdru@gmail.com>
2024-07-02 21:01:22 -0300

    clase 2 - componentes

---
+++ clase-02/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './components/my-header/my-header.component';
+import { MyHeader2Component } from './components/my-header-2/my-header-2.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyHeaderComponent,
+    MyHeader2Component,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ clase-02/src/app/components/my-header-2/my-header-2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-header-2',
+  templateUrl: './my-header-2.component.html'
+})
+export class MyHeader2Component {
+
+}
